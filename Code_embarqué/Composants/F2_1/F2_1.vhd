af8f682c03ea19e7fa8a15e7d77273bab4d126b9
NicolasO-git <nicolas.plaisance@hotmail.fr>
2019-09-29 23:50:45 +0200

    initialisation F2_1

---
+++ Code_embarqué/Composants/F2_1/F2_1.vhd
@@ -0,0 +1,18 @@
+library IEEE;
+use IEEE.std_logic_1164.all;
+use IEEE.numeric_std.all;
+
+entity F2_1 is
+    port 
+    (
+            
+    );
+end entity F2_1;
+
+architecture rtl of F2_1 is
+    
+begin
+    
+    
+    
+end architecture rtl;
