356dab605203aebab117eeefc44449a4294d84e8
Etudiant <Etudiant@U4-305p6.ups-tlse.fr>
2019-10-03 08:16:48 +0200

    Maj SOPC PWM

--- Code_embarqué/SOPC/sopc_add_qip_file.tcl
+++
@@ -1,3 +0,0 @@
-project_open -current_revision "C:/Users/Etudiant/Documents/ROUTIER_OTAL_FPGA_M2/Barre_Franche/Code_embarqué/SOPC/Sopc_compteur"
-set_global_assignment -name QIP_FILE "config_sopc.qip"
-project_close
